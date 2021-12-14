ab05caebdd66b66bea583f2ded70f47cb8b0c3cc
SleeplessCoder09 <95735495+SleeplessCoder09@users.noreply.github.com>
2021-12-15 01:26:10 +0530

    Create BookWishList
    First Project on GitHub

---
+++ BookWishList.cpp
@@ -0,0 +1,69 @@
+//Console-Based Book WishList Application and when you number of lines of code ;)
+#include<iostream>
+#include<string>
+#include<string.h>
+#include<conio.h>
+using namespace std;
+class DataBook{
+private:
+int length=0;
+char book_name[100][100];
+char author[100][100];
+bool isOwned[100];
+public:
+void get_info()
+{
+    char own;
+    cout<<"Enter Name Of Book(100 characters limit): ";
+    cin.getline(book_name[length],100);//Why I have to do that can anyone explain to me
+    cin.getline(book_name[length],100);
+    cout<<"Enter Author Of Book(100 characters limit): ";
+    cin.getline(author[length],100);
+    cout<<"Enter do you have book?(Y/N): ";
+    cin>>own;
+    if(own=='Y' || own=='y')
+        isOwned[length]=true;
+    else if(own=='N' || own=='n')
+        isOwned[length]=false;
+    else
+        cout<<"It is been Empty";
+    this->length++;
+}
+void print_info()
+{
+    int i;
+    for(i=0;i<length;i++)
+    {
+        cout<<book_name[i]<<"\t"<<author[i]<<"\t";
+        if(isOwned[i])
+            cout<<"Owned";
+        else
+            cout<<"Not Owned";
+        cout<<endl;
+    }
+}
+};
+int main()
+{
+    DataBook d1;
+    int inp;
+    while(true)
+    {
+        cout<<"1:Enter A Entry\t2:Print the List\n3:Quit the Application ";
+        cin>>inp;
+        if(inp==1)
+        {
+            d1.get_info();
+        }
+        else if(inp==2)
+        {
+            d1.print_info();
+        }
+        else if(inp==3)
+        {
+            break;
+        }
+        else if(inp>3)
+            cout<<"Enter Again";
+    }
+}
