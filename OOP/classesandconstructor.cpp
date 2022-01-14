90b53209b9f0b30650462c2f3935c545af1523ef
SleeplessCoder09 <95735495+SleeplessCoder09@users.noreply.github.com>
2022-01-14 19:21:38 +0530

    Update and rename classes.cpp to classesandconstructor.cpp

--- OOP/classesandconstructor.cpp
+++ OOP/classesandconstructor.cpp
@@ -5,6 +5,10 @@ class A
   private: //it is default access specifier
   int a;
   public:
+  A()
+  {
+   int a=0;
+  }
   void setint(int x)
   {
     a=x;
@@ -16,7 +20,7 @@ class A
 }; //Note: ";" is required, it is common mistake that people forgot
 int main()
 {
-  A a1;
+  A a1;//constructor gets called, assigning a to 0
   a1.setint(10);
   a1.getint();
 }
