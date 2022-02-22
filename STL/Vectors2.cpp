9e765fa8189b543c140021c7eef43e6902ac026d
SleeplessCoder09 <95735495+SleeplessCoder09@users.noreply.github.com>
2022-02-22 23:04:14 +0530

    Create Vectors2.cpp

---
+++ STL/Vectors2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> vec;
+    int n,i,temp;
+    cout<<"Enter Number to provide Lenght"<<endl;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>temp;
+        vec.push_back(temp);//Putting Number At End
+    }
+    for(i=0;i<n;i++)
+        cout<<vec[i]<<" ";
+    cout<<endl;
+    int a,b;
+    cout<<"Enter a Range Number to eraser:\n";
+    cin>>a>>b;
+    vec.erase(vec.begin()+a,vec.begin()+b);
+    for(i=0;i<vec.size();i++)
+        cout<<vec[i]<<" ";
+    cout<<endl;
+    return 0;
+}
