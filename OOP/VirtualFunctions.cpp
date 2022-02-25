0aaeaa6e380595f98492afbd27f7d19a8e654b14
SleeplessCoder09 <95735495+SleeplessCoder09@users.noreply.github.com>
2022-02-25 23:38:43 +0530

    Create VirtualFunctions.cpp

---
+++ OOP/VirtualFunctions.cpp
@@ -0,0 +1,92 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Person
+{
+    protected:
+    string name;
+    int age;  
+    public:
+    virtual void getdata()
+    {
+        cout<<"virtual";
+    }
+    virtual void putdata()
+    {
+        cout<<"virtual";
+    }
+};
+class Professor:public Person
+{
+    private:
+    int publications;
+    static int cur_id;
+    public:
+    void getdata()
+    {
+        cin>>name;
+        cin>>age>>publications;
+    }
+    void putdata()
+    {
+        cout<<name<<" "<<age<<" "<<publications<<" "<<cur_id<<endl;
+        cur_id++;
+    }
+};
+int Professor::cur_id=1;
+class Student: public Person
+{
+    private:
+    int marks[6];
+    static int cur_id;
+    public:
+    void getdata()
+    {
+        cin>>name;
+        cin>>age;
+        for(int i =0;i<6;i++)
+        {
+            cin>>marks[i];
+        }
+    }
+    void putdata()
+    {
+        cout<<name<<" "<<age;
+        int sum=0;
+        for(int i=0;i<6;i++)
+        {
+            sum+=marks[i];
+        }
+        cout<<" "<<sum<<" "<<cur_id<<endl;
+        cur_id++;
+    }
+};
+int Student::cur_id=1;
+int main(){
+
+    int n, val;
+    cin>>n;
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student;
+
+        per[i]->getdata();
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata();
+
+    return 0;
+
+}
